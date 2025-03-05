84d24744b88d7e4acf1c9294b567ead409c5e9d0
KJH04_ <10wjdgp04@gmail.com>
2025-03-05 13:25:30 +0900

    Update 10250backjoon.py

--- 10250backjoon.py
+++ 10250backjoon.py
@@ -3,16 +3,13 @@ floor = []
 room_num = []
 
 for _ in range(t):
-    while(1):
         h, w, n = map(int,input().split())
-        if h >= 1 and w <=99 and n >=1 and (n <= h * w):
-            if n%h != 0:
-                floor.append(n%h)
-                room_num.append(n//h + 1)
-            else:
-                floor.append(h)
-                room_num.append(n//h)
-            break
+        if n%h != 0:
+            floor.append(n%h)
+            room_num.append(n//h + 1)
+        else:
+            floor.append(h)
+            room_num.append(n//h)
 
 for i in range(t):
     if room_num[i] > 9: print(f'{floor[i]}{room_num[i]}')
