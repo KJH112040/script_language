241b9176d50fc259b42be7321012b7cd5c0ce877
KJH04_ <10wjdgp04@gmail.com>
2025-03-06 11:10:51 +0900

    Update 10250backjoon.py

--- 10250backjoon.py
+++ 10250backjoon.py
@@ -14,3 +14,16 @@ for _ in range(t):
 for i in range(t):
     if room_num[i] > 9: print(f'{floor[i]}{room_num[i]}')
     else: print(f'{floor[i]}0{room_num[i]}')
+
+#교수님 코드
+#def reserve(H,W,N):    #H,W,N에 대한 방번호 반환 6 12 10
+#   count = 1
+#   for room in range(1,W+1):
+#       for floor in range(1,H+1):
+#           if count == N:
+#               return floor * 100 + room
+#           count += 1
+#T = int(input())   #Test case 개수
+#for _ in range(T)
+#   H,W,N = map(int,input().split())
+#   print(reserve(H,W,N))
