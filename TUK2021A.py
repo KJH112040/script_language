97dc47719f94a49710f8935cdc9ef7d705760808
KJH04_ <10wjdgp04@gmail.com>
2025-03-12 10:44:13 +0900

    Update TUK2021A.py

--- TUK2021A.py
+++ TUK2021A.py
@@ -1,10 +1,26 @@
+#======================================================
+# STR = input()
+# bomb = input()
+# output = []
+#
+# for c in STR:
+#     output.append(c)
+#     if ''.join(output[-len(bomb):]) == bomb:
+#         del output[-len(bomb):]
+#
+# print(''.join(output))
+#======================================================
 STR = input()
 bomb = input()
-output = []
 
-for c in STR:
-    output.append(c)
-    if ''.join(output[-len(bomb):]) == bomb:
-        del output[-len(bomb):]
+while bomb in STR:
+    STR = STR.replace(bomb,'')
 
-print(''.join(output))
+print(''.join(STR))
+
+#=============교수님 코드================
+#s = input()
+#e = input()
+#while e in s:
+#   s = s.replace(e,'')
+#print(s)
